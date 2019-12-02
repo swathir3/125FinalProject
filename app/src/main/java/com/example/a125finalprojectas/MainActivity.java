d7f5ee4c55c351697d9418faa8b74f7e9ec6cf7f
Swathi Ram <swathir3@illinois.edu>
2019-12-02 17:59:34 -0600

    bouncing ball, circle, ball activity, working on emulator

--- app/src/main/java/com/example/a125finalprojectas/MainActivity.java
+++ app/src/main/java/com/example/a125finalprojectas/MainActivity.java
@@ -2,19 +2,21 @@ package com.example.a125finalprojectas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.Button;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private Button myButton;
+    Bouncingball bbview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.StartGame);
-        button.setOnClickListener(new View.OnClickListener() {
+        myButton = findViewById(R.id.StartGame);
+        myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openGame();
@@ -23,8 +25,11 @@ public class MainActivity extends AppCompatActivity {
     }
     // button set to open game class
     public void openGame() {
-        Intent intent = new Intent(this, BallActivity.class);
+        bbview = new Bouncingball(this);
+        setContentView(bbview);
+        bbview.setBackgroundColor(Color.BLACK);
+        /*Intent intent = new Intent(this, BallActivity.class);
         startActivity(intent);
-        finish();
+        finish();*/
     }
 }
