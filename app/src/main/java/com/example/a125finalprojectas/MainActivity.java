3a5eaa051c868f588baed0b19db56babf7539b49
Swathi Ram <swathir3@illinois.edu>
2019-12-02 17:09:33 -0600

    final project ui

--- app/src/main/java/com/example/a125finalprojectas/MainActivity.java
+++ app/src/main/java/com/example/a125finalprojectas/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.a125finalprojectas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.StartGame);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGame();
+            }
+        });
+    }
+    // button set to open game class
+    public void openGame() {
+        Intent intent = new Intent(this, NewGameActivity.class);
+        startActivity(intent);
     }
 }
