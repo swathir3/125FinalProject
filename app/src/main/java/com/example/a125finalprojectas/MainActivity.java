d350540d8f6b316a09ba34d255fb1c11cb6fb5bd
Swathi Ram <swathir3@illinois.edu>
2019-12-05 21:20:58 -0600

    timer

--- app/src/main/java/com/example/a125finalprojectas/MainActivity.java
+++ app/src/main/java/com/example/a125finalprojectas/MainActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.CountDownTimer;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.content.Intent;
 import android.widget.Button;
 import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final long START_TIME = 30000;
+    private TextView CountDown;
     private Button myButton;
     private SoundPlayer sound;
+    private CountDownTimer myCountDownTimer;
+    private boolean myTimerRunning;
+    private long myTimeLeft = START_TIME;
     Bouncingball bbview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
                 openGame();
             }
         });
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer();
+            }
+        });
+        CountDown = findViewById(R.id.text_view_countdown);
     }
     // button set to open game class
     public void openGame() {
@@ -34,6 +47,18 @@ public class MainActivity extends AppCompatActivity {
         /*Intent intent = new Intent(this, BallActivity.class);
         startActivity(intent);
         finish();*/
+    }
+    public void startTimer() {
+        myCountDownTimer = new CountDownTimer(myTimeLeft, 1000) {
+            @Override
+            public void onTick(long sUntilFinished) {
+                myTimeLeft = sUntilFinished;
+                CountDown.setText("timer " + sUntilFinished / 1000);
+            }
+            public void onFinish() {
+                CountDown.setText("game over");
+            }
+        }.start();
     }
     public void touchCheck() {
         //set sound to geoff heads and incorrect heads
